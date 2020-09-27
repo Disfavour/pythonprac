11592b2d2b8b99fb053906f2247095238df9ecdd
Disfavour <disfavour228@gmail.com>
2020-09-27 17:30:10 +0300

    task3 added

--- 20200924_3/task_3.py
+++ 20200924_3/task_3.py
@@ -1,1 +1,17 @@
-q
+a = input()
+q = []
+flag = 0
+if a:
+    while a:
+        q.append(a)
+        a = input()
+for item in tuple(q):
+    a = q.copy()
+    a.remove(item)
+    for item1 in a:
+        if not set(item).isdisjoint(set(item1)):
+            flag = 1
+            break
+    if not flag:
+        print(item)
+    flag = 0
