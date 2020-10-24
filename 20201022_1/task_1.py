1959da606cbf78047ecb2928c6e56ad6c072b22d
Disfavour <disfavour228@gmail.com>
2020-10-24 14:11:18 +0300

    homework

---
+++ 20201022_1/task_1.py
@@ -0,0 +1,16 @@
+def fib(m, n):
+    k = [1, 1]
+    i = m
+    while m <= i <= n:
+        if i == 1 or i == 2:
+            yield 1
+            i += 1
+            continue
+        yield sum(k)
+        k.reverse()
+        k[1] = sum(k)
+        i += 1
+
+
+print(list(fib(*eval(input()))))
+
--- 20201022_3/task_3.py
+++ 20201022_3/task_3.py
@@ -1,4 +1,5 @@
-from itertools import*
-n = input()
-product("TOR", n)
+from itertools import *
+
+
+print(list(filter(lambda x: "".join(x).count("TOR") == 2, product('ORT', repeat=int(input())))))
 
