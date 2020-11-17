1b6723f2d254b86a73d7fd47c0add7fe2d8276d4
Disfavour <disfavour228@gmail.com>
2020-11-18 01:21:05 +0300

    with tests

--- 20201112_2/task_2.py
+++ 20201112_2/task_2.py
@@ -1,8 +1,13 @@
+from collections import *
+
+
 class nestr(UserString):
     def __neg__(self):
-        self.data = self.__class__("".join(str(self.data).reversed()))
-        return self.data
-    a = "qwerty"
-    b = "asdfgh"
-print(eval(input()  )
-print(type(eval(input())))
+        self.data = ("".join(reversed(list(self.data))))
+        return self.__class__(self.data)
+
+
+a = nestr("aabbcdefg")
+b = nestr("abbbklopafdsdg")
+print(eval(input()))
+
--- 20201112_3/task_3.py
+++ 20201112_3/task_3.py
@@ -1,10 +1,20 @@
 while True:
-    s = input()
     try:
-        n = float(s)
+        (x1, y1), (x2, y2), (x3, y3) = eval(input())
     except:
-        print("Try again")
+        print("Invalid input")
     else:
+        a = ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
+        b = ((x1 - x3) ** 2 + (y1 - y3) ** 2) ** 0.5
+        c = ((x3 - x2) ** 2 + (y3 - y2) ** 2) ** 0.5
+        p = (a + b + c) / 2
+
+        S = (p * (p - a) * (p - b) * (p - c)) ** 0.5
+
+        if S == 0:
+            print("Not a triangle")
+            continue
+        else:
+            print(f'{S:.{2}f}')
         break
-print(n)
 
