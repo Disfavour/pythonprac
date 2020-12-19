594cab914f6f4d3c87891604e03daf5a4e76719a
Disfavour <disfavour228@gmail.com>
2020-12-19 22:02:56 +0300

    update

--- 20201119_1/task_1.py
+++ 20201119_1/task_1.py
@@ -2,11 +2,11 @@ import sys
 
 def deccount(f):
     __a = 0
-    def newfun(*args):
+    def newfun(*args, **kwargs):
         nonlocal __a
         __a += 1
         print(__a)
-        return f(*args)
+        return f(*args, **kwargs)
     return newfun
 
 
