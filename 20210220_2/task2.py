20942d752b03b492d49e976c8d9e53e881ae5624
Disfavour <disfavour228@gmail.com>
2021-02-26 18:23:02 +0300

    Added git button

--- 20210220_2/task2.py
+++ 20210220_2/task2.py
@@ -12,12 +12,14 @@ class Application(tk.Frame):
     def createWidgets(self):
         self.dir_button = tk.Button(self, text="dir", command=self.dir_func)
         self.date_button = tk.Button(self, text="date", command=self.date_func)
+        self.git_button = tk.Button(self, text="git", command=self.git_func)
         self.quitButton = tk.Button(self, text='Quit', command=self.quit)
 
         self.funcplace = tk.Label(self, textvariable=self.x)
 
         self.dir_button.grid()
         self.date_button.grid()
+        self.git_button.grid()
         self.funcplace.grid()
         self.quitButton.grid()
 
@@ -27,6 +29,10 @@ class Application(tk.Frame):
 
     def date_func(self):
         process = subprocess.run("date", capture_output=True)
+        self.x.set(process.stdout.decode()[:-1])
+
+    def git_func(self):
+        process = subprocess.run("git", capture_output=True)
         self.x.set(process.stdout.decode()[:-1])
 
 
