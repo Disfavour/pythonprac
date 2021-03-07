e39f22f5a10e5ae2727dcec9360f6a9213ee838c
Disfavour <disfavour228@gmail.com>
2021-03-07 16:30:45 +0300

    Exit button

--- 20210301_1/task1.py
+++ 20210301_1/task1.py
@@ -5,6 +5,15 @@ class Application(tkinter.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.grid()
+        self.createWidgets()
+
+    def createWidgets(self):
+        self.quit_button = tkinter.Button(self, text='Quit', command=self.quit_button_func)
+        self.quit_button.grid()
+
+    def quit_button_func(self):
+        """Завершает выполнение программы"""
+        pass
 
 
 app = Application()
