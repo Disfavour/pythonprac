ae19495e8a6aee93e303a8d9d3321b15f062ab2c
Disfavour <disfavour228@gmail.com>
2021-03-07 16:34:59 +0300

    Options menu

--- 20210301_1/task1.py
+++ 20210301_1/task1.py
@@ -8,13 +8,19 @@ class Application(tkinter.Frame):
         self.createWidgets()
 
     def createWidgets(self):
+        self.choice = tkinter.StringVar()
+        self.option_list = ('One', 'Two', 'Three')
+        self.choice.set(self.option_list[0])
+
         self.quit_button = tkinter.Button(self, text='Quit', command=self.quit_button_func)
         self.label_1 = tkinter.Label(self, text="<MenuItem>")
         self.next_item_button = tkinter.Button(self, text='Next item', command=self.next_item_handler)
+        self.drop_down = tkinter.OptionMenu(self, self.choice, *self.option_list)
 
         self.quit_button.grid()
         self.label_1.grid()
         self.next_item_button.grid()
+        self.drop_down.grid()
 
     def quit_button_func(self):
         """Завершает выполнение программы"""
