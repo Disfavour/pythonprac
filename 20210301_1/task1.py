3765fa2595144aa3713281af3d6ec1cc072a9bd1
Disfavour <disfavour228@gmail.com>
2021-03-07 16:33:40 +0300

    Next button

--- 20210301_1/task1.py
+++ 20210301_1/task1.py
@@ -10,12 +10,18 @@ class Application(tkinter.Frame):
     def createWidgets(self):
         self.quit_button = tkinter.Button(self, text='Quit', command=self.quit_button_func)
         self.label_1 = tkinter.Label(self, text="<MenuItem>")
+        self.next_item_button = tkinter.Button(self, text='Next item', command=self.next_item_handler)
 
         self.quit_button.grid()
         self.label_1.grid()
+        self.next_item_button.grid()
 
     def quit_button_func(self):
         """Завершает выполнение программы"""
+        pass
+
+    def next_item_handler(self):
+        """Обработчик нажатия кнопки Next item"""
         pass
 
 
