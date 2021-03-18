27834093a8075b3e692ba39236b99a969d6aaa84
Disfavour <disfavour228@gmail.com>
2021-03-18 14:05:53 +0300

    first task complete

--- 20210315_1/task1.py
+++ 20210315_1/task1.py
@@ -1,11 +1,15 @@
 import tkinter as tk
-from itertools import product
+import re
+
+
+full_num = re.compile(r"[-+]?(?:(?:\d*\.\d+)|(?:\d+\.?))?", re.ASCII)
+
 
 class Application(tk.Frame):
-    '''Sample tkinter application class'''
+    """Sample tkinter application class"""
 
     def __init__(self, master=None, title="<application>", **kwargs):
-        '''Create root window with frame, tune weight and resize'''
+        """Create root window with frame, tune weight and resize"""
         super().__init__(master, **kwargs)
         self.master.title(title)
         self.master.columnconfigure(0, weight=1)
@@ -18,23 +22,43 @@ class Application(tk.Frame):
                 self.rowconfigure(row, weight=1)
 
     def create_widgets(self):
-        '''Create all the widgets'''
+        """Create all the widgets"""
+
 
 class App(Application):
     def create_widgets(self):
-        alpha = self.register(self.alpha)
+        num = self.register(self.num)
         self.S = tk.StringVar()
-        self.E = tk.Entry(self, textvariable=self.S,
-                validate='all', validatecommand=(alpha, '%V', '%S'))
+
+        self.E = tk.Entry(self, textvariable=self.S, validate='all', validatecommand=(num, "%P", "%S", "%V"))
         self.E.grid(columnspan=2)
-        self.L = tk.Label(self, text="Lower letters only")
+
+        self.L = tk.Label(self, text="only int or float")
         self.L.grid(row=1, column=0)
-        self.Q = tk.Button(self, text="Quit", command=self.master.quit)
+
+        self.Q = tk.Button(self, text="Quit", command=self.big_quit)
         self.Q.grid(row=1, column=1)
 
-    def alpha(self, why, txt):
-        print(f"{why}: '{txt}'")
-        return why != 'key' or txt.isalpha() and txt.islower()
+    def big_quit(self):
+        answer = self.S.get()
+        if answer in ["", "-", "+"]:
+            print(0)
+        else:
+            print(eval(answer))
+        self.quit()
+
+    def num(self, new, text, act):
+        #print(new, text, act)
+        if act == "key":
+            matching = full_num.fullmatch(new)
+            #print(matching)
+            if matching:
+                return True
+            else:
+                return False
+        else:
+            return True
+
 
 app = App(title="Sample application")
 app.mainloop()
